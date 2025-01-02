a9249b89038f2d5ce6677fe882c0c2ae752a9abf
anneopesus <anne-sophie.vandendooren@opesus.com>
2025-01-02 19:29:58 +0100

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { ShopItemComponent } from './shop-item/shop-item.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+  ],
+  declarations: [
+    AppComponent,
+    ShopItemComponent
+  ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import { ShopItemComponent } from './shop-item/shop-item.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'shop-item', component: ShopItemComponent },
+  { path: 'second-component', component: SecondComponent },
+  { path: '', redirectTo: '/shop-itemt', pathMatch: 'full' },
+  { path: '**', component: ShopItemComponent },
+];
