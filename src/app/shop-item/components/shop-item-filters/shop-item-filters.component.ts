64731cff4f93f5764af578363da6587b4d70771f
Avdrn <anne.vandendooren@gmail.com>
2025-01-03 13:00:46 +0100

    feat(shopItemFilters): implement UI5 select component for filters

--- src/app/shop-item/components/shop-item-filters/shop-item-filters.component.ts
+++ src/app/shop-item/components/shop-item-filters/shop-item-filters.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
-
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import '@ui5/webcomponents/dist/Option';
+import '@ui5/webcomponents/dist/Select';
 @Component({
   selector: 'shop-item-filters',
   templateUrl: './shop-item-filters.component.html',
   styleUrls: ['./shop-item-filters.component.scss'],
   standalone: true,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class ShopItemFiltersComponent { }
